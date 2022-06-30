10a31bbfb3c893058c9af573da7b74d3bd5fefcf
VD <vdfree448@protonmail.com>
2022-06-30 15:39:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/module_1_lesson_1_hw_2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.module_1_lesson_1_hw_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("MYLOG", "$$$$$$$$$$");
+        Log.d("MYLOG", "$$$$  $$$$");
+        Log.d("MYLOG", "$$      $$");
+        Log.d("MYLOG", "$  $$$$  $");
+        Log.d("MYLOG", "$  $$$$$$$");
+        Log.d("MYLOG", "$$      $$");
+        Log.d("MYLOG", "$$$$$$$  $");
+        Log.d("MYLOG", "$  $$$$  $");
+        Log.d("MYLOG", "$$      $$");
+        Log.d("MYLOG", "$$$$  $$$$");
+        Log.d("MYLOG", "$$$$$$$$$$");
+
+    }
+}
